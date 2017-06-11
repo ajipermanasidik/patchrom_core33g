6ce5587f6a99cc218fe31d3ac84447f7c41afffd
HA$HT@G <hasherror404@gmail.com>
2017-06-11 11:23:11 +0530

    fix build error
    Signed-off-by: HA$HT@G <hasherror404@gmail.com>

--- customize_target_files.sh
+++ customize_target_files.sh
@@ -6,5 +6,11 @@ TARGET_FILES_DIR=$OUT_DIR/target_files
 build_prop_file=$TARGET_FILES_DIR/SYSTEM/build.prop
 OTHER_DIR=$PWD/other
 
+if [ -e $METADATA_DIR/updater ]
+then
+   echo "Use custom updater bin file"
+   cp $METADATA_DIR/updater $TARGET_FILES_DIR/OTA/bin
+fi
+
 cp -f other/file_contexts out/target_files/META/
 rm -rf out/target_files/SYSTEM/vendor/preinstall
