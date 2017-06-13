8daf5665b0e58fbf82bbebf09b9af5fb82a00537
HA$HT@G <hasherror404@gmail.com>
2017-06-13 08:24:37 +0530

    initial commit
    Signed-off-by: HA$HT@G <hasherror404@gmail.com>

---
+++ phoneapps.mk
@@ -0,0 +1,37 @@
+local-phone-apps = $(private-phone-apps)
+local-phone-priv-apps = $(private-phone-priv-apps)
+
+private-phone-apps := \
+	BasicDreams \
+	Bluetooth \
+	BluetoothMidiService \
+	CertInstaller \
+	KeyChain \
+	LatinIME \
+	LiveWallpapers \
+	PacProcessor \
+	PicoTts \
+	PrintSpooler \
+	Stk \
+	TimeService \
+	telresources \
+	WAPPushManager
+
+private-phone-priv-apps := \
+	AudioFX \
+	BackupRestoreConfirmation \
+	CarrierConfig \
+	CellBroadcastReceiver \
+	CMAudioService \
+	CMSettingsProvider \
+	ExternalStorageProvider \
+	FusedLocation \
+	InputDevices \
+	OneTimeInitializer \
+	ProxyHandler \
+	SharedStorageBackup \
+	Shell \
+	Screencast \
+	StatementService \
+	ThemesProvider
+
